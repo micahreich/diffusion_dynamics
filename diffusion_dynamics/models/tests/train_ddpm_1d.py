9d260856177ca85f4fbddf5ff46a9ea322d054ef
Micah Reich <micahreich02@gmail.com>
2025-02-13 02:47:29 -0500

    edit arch

--- diffusion_dynamics/models/tests/train_ddpm_1d.py
+++ diffusion_dynamics/models/tests/train_ddpm_1d.py
@@ -15,8 +15,9 @@ class ExampleModel(UNet1DModel):
             out_channels=n_channels,
             base_channels=32,
             dim_mults=[1, 2, 4],
-            attention=False,
+            attention=True,
         )
+        
         scheduler = DDPMScheduler(num_train_timesteps=1000, clip_sample=False, variance_type="fixed_small_log")
 
         super().__init__(unet, scheduler, n_channels)
